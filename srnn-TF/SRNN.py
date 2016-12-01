e9c31336dc35319a4ffd7fd93e3e88279a2fbd67
Sephora Madjiheurem <msephora@vslab101.ethz.ch>
2016-12-01 18:06:49 +0100

    updated model, fixed dimensions issues

--- srnn-TF/SRNN.py
+++ srnn-TF/SRNN.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 from tensorflow.python.ops import rnn, rnn_cell
 
-class SRNN(object):
+class SRNN_model(object):
 
     # SharedRNN(shared_layers, human_layers, object_layers, softmax_loss, trY_1, trY_2, 1e-3)
 
-    def __int__(self,temp_inputs, st_inputs, num_classes, num_frames ,num_units, max_gradient_norm, batch_size, learning_rate,
+    def __int__(self, num_classes, num_frames ,num_units, max_gradient_norm, batch_size, learning_rate,
                                        learning_rate_decay_factor, adam_epsilon,  GD, forward_only=False, l2_regularization=False, weight_decay=0):
         """
         Create S-RNN model
@@ -25,125 +25,184 @@ class SRNN(object):
         return:
         """
 
+
         self.batch_size = batch_size
         self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
         self.learning_rate_decay_op = self.learning_rate.assign(
             self.learning_rate * learning_rate_decay_factor)
+        self.max_grad_norm = max_gradient_norm
         self.global_step = tf.Variable(0, trainable=False)
         self.adam_epsilon = adam_epsilon
         self.GD = GD
         self.weight_decay = weight_decay
 
-        input_F, input_rA, input_lA, input_lL, input_rL, input_B = temp_inputs
-        input_FlA, input_FrA, input_FB, input_BlA, input_BrA, input_BlL, input_BrL = st_inputs
+        # TODO: replace by placeholders
+        temp_features_names = ['face-face','rightArm-rightArm','leftArm-leftArm','rightLeg-rightLeg','leftLeg-leftLeg','belly-belly']
+        st_features_names = ['face-leftArm','face-rightArm','face-belly','belly-leftArm','belly-rightArm',
+                             'belly-rightLeg','belly-leftLeg']
+        nodes_names = {'face','arms','legs','belly'}
+        edgesRNN = {}
+        nodesRNN = {}
+        states = {}
+        self.inputs = {}
+        self.targets = tf.placeholder(tf.float32, shape=[None], name='targets')
+
+        for temp_feat in temp_features_names:
+            self.inputs[temp_feat] = tf.placeholder(tf.float32, shape=[None], name=temp_feat)
+            edgesRNN[temp_feat] = tf.nn.rnn_cell.LSTMCell(num_units)
+            states[temp_feat] = tf.zero_state([batch_size, edgesRNN[temp_feat].state_size])
+
+        for st_feat in st_features_names:
+            self.inputs[st_feat] = tf.placeholder(tf.float32, shape=[None], name=st_feat)
+            edgesRNN[st_feat] = tf.nn.rnn_cell.LSTMCell(num_units)
+            states[st_feat] = tf.zero_state([batch_size, edgesRNN[st_feat].state_size])
+
+        for node in nodes_names:
+            self.inputs[node] = tf.placeholder(tf.float32, shape=[None], name=node)
+            nodesRNN[node] = tf.nn.rnn_cell.LSTMCell(num_units)
+            states[node] = tf.zero_state([batch_size, nodesRNN[node].state_size])
+
+
+        # input_F, input_rA, input_lA, input_lL, input_rL, input_B = temp_inputs
+        # input_FlA, input_FrA, input_FB, input_BlA, input_BrA, input_BlL, input_BrL = st_inputs
 
         weights = {'out' : tf.Variable(tf.random_normal([num_units,num_classes]))}
         biases = {'out' : tf.Variable(tf.random_normal([num_classes]))}
 
-        # TODO: please re-write in a more elegant way, thank you
-        edgesRNN = {}
-        states = {}
+        # # temporal edges
+        # edgesRNN['face-face'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['face-face'] = tf.zero_state([batch_size, edgesRNN['face-face'].state_size])
+        #
+        # edgesRNN['rightArm-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['rightArm-rightArm'] = tf.zero_state([batch_size, edgesRNN['rightArm-rightArm'].state_size])
+        #
+        # edgesRNN['leftArm-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['leftArm-leftArm'] = tf.zero_state([batch_size, edgesRNN['leftArm-leftArm'].state_size])
+        #
+        # edgesRNN['leftLeg-leftLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['leftLeg-leftLeg'] = tf.zero_state([batch_size, edgesRNN['leftLeg-leftLeg'].state_size])
+        #
+        # edgesRNN['rightLeg-rightLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['rightLeg-rightLeg'] = tf.zero_state([batch_size, edgesRNN['rightLeg-rightLeg'].state_size])
+        #
+        # edgesRNN['belly-belly'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly-belly'] = tf.zero_state([batch_size, edgesRNN['belly-belly'].state_size])
 
-        # temporal edges
-        edgesRNN['face-face'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['face-face'] = tf.zero_state([batch_size, edgesRNN['face-face'].state_size])
+        # # spatio edges
+        # edgesRNN['face-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['face-leftArm'] = tf.zero_state([batch_size, edgesRNN['face-leftArm'].state_size])
+        #
+        # edgesRNN['face-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['face-rightArm'] = tf.zero_state([batch_size, edgesRNN['face-rightArm'].state_size])
+        #
+        # edgesRNN['face-belly'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['face-belly'] = tf.zero_state([batch_size, edgesRNN['face-belly'].state_size])
+        #
+        # edgesRNN['belly-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly-leftArm'] = tf.zero_state([batch_size, edgesRNN['belly-leftArm'].state_size])
+        #
+        # edgesRNN['belly-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly-rightArm'] = tf.zero_state([batch_size, edgesRNN['belly-rightArm'].state_size])
+        #
+        # edgesRNN['belly-leftLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly-leftLeg'] = tf.zero_state([batch_size, edgesRNN['belly-leftLeg'].state_size])
+        #
+        # edgesRNN['belly-rightLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly-rightLeg'] = tf.zero_state([batch_size, edgesRNN['belly-rightLeg'].state_size])
 
-        edgesRNN['rightArm-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['rightArm-rightArm'] = tf.zero_state([batch_size, edgesRNN['rightArm-rightArm'].state_size])
-
-        edgesRNN['leftArm-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['leftArm-leftArm'] = tf.zero_state([batch_size, edgesRNN['leftArm-leftArm'].state_size])
-
-        edgesRNN['leftLeg-leftLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['leftLeg-leftLeg'] = tf.zero_state([batch_size, edgesRNN['leftLeg-leftLeg'].state_size])
-
-        edgesRNN['rightLeg-rightLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['rightLeg-rightLeg'] = tf.zero_state([batch_size, edgesRNN['rightLeg-rightLeg'].state_size])
-
-        edgesRNN['belly-belly'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly-belly'] = tf.zero_state([batch_size, edgesRNN['belly-belly'].state_size])
-
-        #spatio edges
-        edgesRNN['face-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['face-leftArm'] = tf.zero_state([batch_size, edgesRNN['face-leftArm'].state_size])
-
-        edgesRNN['face-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['face-rightArm'] = tf.zero_state([batch_size, edgesRNN['face-rightArm'].state_size])
-
-        edgesRNN['face-belly'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['face-belly'] = tf.zero_state([batch_size, edgesRNN['face-belly'].state_size])
-
-        edgesRNN['belly-leftArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly-leftArm'] = tf.zero_state([batch_size, edgesRNN['belly-leftArm'].state_size])
-
-        edgesRNN['belly-rightArm'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly-rightArm'] = tf.zero_state([batch_size, edgesRNN['belly-rightArm'].state_size])
-
-        edgesRNN['belly-leftLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly-leftLeg'] = tf.zero_state([batch_size, edgesRNN['belly-leftLeg'].state_size])
-
-        edgesRNN['belly-rightLeg'] = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly-rightLeg'] = tf.zero_state([batch_size, edgesRNN['belly-rightLeg'].state_size])
-
-        # nodes
-        nodesRNN = {}
-        nodesRNN['face']  = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['face'] = tf.zero_state(batch_size, nodesRNN[''].state_size)
-
-        nodesRNN['arms']  = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['arms'] = tf.zero_state(batch_size, nodesRNN['arms'].state_size)
-
-        nodesRNN['legs']  = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['legs'] = tf.zero_state(batch_size, nodesRNN['legs'].state_size)
-
-        nodesRNN['belly']  = tf.nn.rnn_cell.LSTMCell(num_units)
-        states['belly'] = tf.zero_state(batch_size, nodesRNN['belly'].state_size)
+        # # nodes
+        # nodesRNN['face']  = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['face'] = tf.zero_state(batch_size, nodesRNN['face'].state_size)
+        #
+        # nodesRNN['arms']  = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['arms'] = tf.zero_state(batch_size, nodesRNN['arms'].state_size)
+        #
+        # nodesRNN['legs']  = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['legs'] = tf.zero_state(batch_size, nodesRNN['legs'].state_size)
+        #
+        # nodesRNN['belly']  = tf.nn.rnn_cell.LSTMCell(num_units)
+        # states['belly'] = tf.zero_state(batch_size, nodesRNN['belly'].state_size)
 
         fullbodyRNN = tf.nn.rnn_cell.LSTMCell(num_units)
         states['fullbody'] = tf.zero_state(batch_size, fullbodyRNN.state_size)
 
+        outputs = []
+        final_outputs = []
 
+        node_inputs = {}
+        node_outputs = {}
         # connect the edgesRNN to the corresponding nodeRNN
         with tf.variable_scope("SRNN"):
             #TODO: proceed in batches!
             for time_step in range(num_frames):
-                output_FF, states['face-face'] = edgesRNN['face-face'](input_F[:,time_step], states['face-face'])
-                output_rArA, states['rightArm-rightArm'] = edgesRNN['rightArm-rightArm']( input_rA[:,time_step], states['rightArm-rightArm'] )
-                output_lAlA, states['leftArm-leftArm'] = edgesRNN['leftArm-leftArm']( input_lA[:,time_step], states['leftArm-leftArm'] )
-                output_lLlL, states['leftLeg-leftLeg'] = edgesRNN['leftLeg-leftLeg']( input_lL[:,time_step],states['leftLeg-leftLeg'] )
-                output_rLrL, states['rightLeg-rightLeg'] = edgesRNN['rightLeg-rightLeg']( input_rL[:,time_step],states['rightLeg-rightLeg'] )
-                output_BB, states['belly-belly'] = edgesRNN['belly-belly']( input_B[:,time_step],states['belly-belly'] )
+                for temp_feat in temp_features_names:
+                    outputs[temp_feat], states[temp_feat] = edgesRNN[temp_feat](self.inputs[temp_feat][:,time_step], states[temp_feat])
+                    # output_rArA, states['rightArm-rightArm'] = edgesRNN['rightArm-rightArm']( input_rA[:,time_step], states['rightArm-rightArm'] )
+                    # output_lAlA, states['leftArm-leftArm'] = edgesRNN['leftArm-leftArm']( input_lA[:,time_step], states['leftArm-leftArm'] )
+                    # output_lLlL, states['leftLeg-leftLeg'] = edgesRNN['leftLeg-leftLeg']( input_lL[:,time_step],states['leftLeg-leftLeg'] )
+                    # output_rLrL, states['rightLeg-rightLeg'] = edgesRNN['rightLeg-rightLeg']( input_rL[:,time_step],states['rightLeg-rightLeg'] )
+                    # output_BB, states['belly-belly'] = edgesRNN['belly-belly']( input_B[:,time_step],states['belly-belly'] )
+                for st_feat in st_features_names:
+                    outputs[st_feat], states[st_feat] = edgesRNN[st_feat](self.inputs[st_feat][:,time_step], states[st_feat])
+                    # output_FlA, states['face-leftArm'] = edgesRNN['face-leftArm']( input_FlA[:,time_step], states['face-leftArm'] )
+                    # output_FrA, states['face-rightArm'] = edgesRNN['face-rightArm']( input_FrA[:,time_step], states['face-rightArm'] )
+                    # output_FB, states['face-belly'] = edgesRNN['face-belly']( input_FB[:,time_step], states['face-belly'] )
+                    # output_BlA, states['belly-leftArm'] = edgesRNN['belly-leftArm']( input_BlA[:,time_step], states['belly-leftArm'] )
+                    # output_BrA, states['belly-rightArm'] = edgesRNN['belly-rightArm']( input_BrA[:,time_step], states['belly-rightArm'] )
+                    # output_BlL, states['belly-leftLeg'] = edgesRNN['belly-leftLeg']( input_BlL[:,time_step], states['belly-leftLeg'] )
+                    # output_BrL, states['belly-rightLeg'] = edgesRNN['belly-rightLeg']( input_BrL[:,time_step], states['belly-rightLeg'] )
 
-                output_FlA, states['face-leftArm'] = edgesRNN['face-leftArm']( input_FlA[:,time_step], states['face-leftArm'] )
-                output_FrA, states['face-rightArm'] = edgesRNN['face-rightArm']( input_FrA[:,time_step], states['face-rightArm'] )
-                output_FB, states['face-belly'] = edgesRNN['face-belly']( input_FB[:,time_step], states['face-belly'] )
-                output_BlA, states['belly-leftArm'] = edgesRNN['belly-leftArm']( input_BlA[:,time_step], states['belly-leftArm'] )
-                output_BrA, states['belly-rightArm'] = edgesRNN['belly-rightArm']( input_BrA[:,time_step], states['belly-rightArm'] )
-                output_BlL, states['belly-leftLeg'] = edgesRNN['belly-leftLeg']( input_BlL[:,time_step], states['belly-leftLeg'] )
-                output_BrL, states['belly-rightLeg'] = edgesRNN['belly-rightLeg']( input_BrL[:,time_step], states['belly-rightLeg'] )
+                node_inputs['face'] = tf.concat(1,[outputs['face-face'], outputs['face-rightArm'], outputs['face-leftArm'], outputs['face-belly']])
+                node_inputs['belly'] = tf.concat(1,[outputs['belly-belly'], outputs['face-belly'], outputs['belly-leftArm'], outputs['belly-rightArm'],
+                                            outputs['belly-leftLeg'], outputs['belly-righLeg']])
+                node_inputs['arms'] = tf.concat(1,[outputs['rightArm-rightArm'], outputs['leftArm-leftArm'], outputs['face-rightArm'],
+                                            outputs['face-leftArm'],outputs['belly-rightArm'], outputs['belly-leftArm']])
+                node_inputs['legs'] = tf.concat(1,[outputs['rightLeg-rightLeg'], outputs['leftLeg-leftLeg'], outputs['belly-rightLeg'], outputs['belly-leftLeg']])
 
-                node_input_F = tf.concat(1,[output_FF, output_FB, output_FlA, output_FrA])
-                node_input_B = tf.concat(1,[output_BB, output_FB, output_BlA, output_BlL, output_BrA, output_BrL])
-                node_input_A = tf.concat(1,[output_rArA,output_lAlA, output_BrA, output_BlA, output_FrA, output_FlA])
-                node_input_L = tf.concat(1,[output_rLrL,output_lLlL, output_BrL, output_BlL])
+                for node_name in nodes_names:
+                    node_outputs[nodes_names] = rnn.rnn(nodesRNN[nodes_names], node_inputs[nodes_names], dtype=tf.float32)
+                    # output_F = rnn.rnn(nodesRNN['face'], node_input_F, dtype=tf.float32)
+                    # output_A = rnn.rnn(nodesRNN['arms'], node_input_A, dtype=tf.float32)
+                    # output_L = rnn.rnn(nodesRNN['legs'], node_input_L, dtype=tf.float32)
+                    # output_B = rnn.rnn(nodesRNN['belly'], node_input_B, dtype=tf.float32)
 
-                output_F = rnn.rnn(nodesRNN['face'], node_input_F, dtype=tf.float32)
-                output_A = rnn.rnn(nodesRNN['arms'], node_input_A, dtype=tf.float32)
-                output_L = rnn.rnn(nodesRNN['legs'], node_input_L, dtype=tf.float32)
-                output_B = rnn.rnn(nodesRNN['belly'], node_input_B, dtype=tf.float32)
+                # fullbody_input = tf.concat(1,[output_F,output_A,output_B,output_L])
+                fullbody_input = tf.concat(1,[node_outputs['face'],node_outputs['belly'],node_outputs['arms'],node_outputs['legs']])
 
-                fullbody_input = tf.concat(1,[output_F,output_A,output_B,output_L])
+                final_output, states['fullbody'] = fullbodyRNN(fullbody_input,states['fullbody'])
+                final_outputs.append(final_output)
 
-                output_Full, states['fullbody'] = fullbodyRNN(fullbody_input,states['fullbody'])
+        output = tf.reshape(tf.concat(1,final_outputs),[-1,num_units])
 
-                self.output = tf.matmul(output_Full, weights['out']) + biases['out']
+        self.final_states = states
+        self.logits = tf.matmul(output, weights['out']) + biases['out']
+        loss = tf.nn.softmax_cross_entropy_with_logits(self.logits, self.targets)
+        self.cost = tf.reduce_mean(loss)
 
+        tvars = tf.trainable_variables()
+        if not forward_only:
+            optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
 
+            clipped_grads, norm = tf.clip_by_global_norm(tf.gradients(self.cost,tvars), self.max_grad_norm)
+            self.gradients_norm = norm
+            self.updates = optimizer.apply_gradients(zip(clipped_grads, tvars),global_step=tf.contrib.framework.get_or_create_global_step())
 
-    def train_model(self,trX_shared_1, trX_1, trY_1, trX_shared_2, trX_2, trY_2, snapshot_rate=1,
-                    path=None, epochs=30, batch_size=50, learning_rate_decay=0.97, decay_after=10):
+        self.saver = tf.train.Saver(tf.all_variables(), max_to_keep=2)
 
-         # Define loss and optimizer
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    def step(self,session,temp_inputs, st_inputs, targets, forward_only):
+        input_feed = {}
 
+        input_feed['temp-inputs'] = temp_inputs;
+        input_feed['st-inputs'] = st_inputs;
+
+        if not forward_only:
+            output_feed = [self.updates, self.gradients_norm,self.cost]
+        else:
+            output_feed = [self.cost, self.logits]
+
+        outputs = session.run(output_feed, input_feed)
+
+        if not forward_only:
+            return outputs[1], outputs[2], None # returs gradients norm and cost and no output
+        else:
+            return outputs[0]
--- srnn-TF/action-recognition-srnn-model.py
+++ srnn-TF/action-recognition-srnn-model.py
@@ -2,98 +2,98 @@ import tensorflow as tf
 import numpy as np
 import sys
 import os
-import cPickle
+import cPickle as pickle
+from SRNN import SRNN_model
 
-def jointModel(num_sub_activities, num_affordances, inputJointFeatures,
-               inputHumanFeatures, inputObjectFeatures):
+tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
+                          "Learning rate decays by this much.")
+tf.app.flags.DEFINE_boolean("l2_reg", False, "Adds a L2 regularization term")
+tf.app.flags.DEFINE_float("adam_epsilon", 1e-6,
+                          "Epsilon used for numerical stability in Adam optimizer.")
+tf.app.flags.DEFINE_float("reg_factor", 1.0,
+                          "Lambda for l2 regulariation.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
+                          "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size", 128,
+                            "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("num_units", 512, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 2, "Number of layers in the model.")
 
-    shared_input_layer = TemporalInputFeatures(inputJointFeatures)
-    shared_hidden_layer = tf.nn.rnn_cell.LSTMCell(128)
+tf.app.flags.DEFINE_integer("num_activities", 2, "Number of decoders, i.e. number of context chords")
 
-    shared_layers = [shared_input_layer, shared_hidden_layer]
+tf.app.flags.DEFINE_string("data_file", "JSB_Chorales.pickle", "Data file name")
 
-    human_layers = [ConcatenateFeatures(inputHumanFeatures), tf.nn.rnn_cell.LSTMCell(256),
-                    'softmax']
+tf.app.flags.DEFINE_boolean("GD", False, "Uses Gradient Descent with adaptive learning rate")
+tf.app.flags.DEFINE_string("train_dir", "bach/JSB_Chorales_128batch_2layers_512units", "Training directory.")
 
-    object_layers = [ConcatenateFeatures(inputObjectFeatures), tf.nn.rnn_cell.LSTMCell(256),
-                     'softmax']
+tf.app.flags.DEFINE_integer("max_train_data_size", 0,
+                            "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("max_valid_data_size", 0,
+                            "Limit on the size of validation data (0: no limit).")
+tf.app.flags.DEFINE_integer("max_test_data_size", 0,
+                            "Limit on the size of validation data (0: no limit).")
+tf.app.flags.DEFINE_integer("max_epochs", 20,
+                            "Maximium number of epochs for trainig.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint",100,
+                            "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("self_test", False,
+                            "Run a self-test if this is set to True.")
+tf.app.flags.DEFINE_boolean("test_model", False,
+                            "Evaluate an existing model on test data")
 
-    trY_1 = T.lmatrix()
-    trY_2 = T.lmatrix()
-    sharedrnn = SharedRNN(shared_layers, human_layers, object_layers, softmax_loss, trY_1, trY_2, 1e-3)
-    return sharedrnn
+FLAGS = tf.app.flags.FLAGS
 
-    return None
+def extract_features(file_name, get_train_data=True, get_valid_data=False, get_test_data=False):
+
+    if not get_train_data ^ get_valid_data ^ get_test_data or get_train_data & get_valid_data & get_test_data:
+        raise ValueError("Only one of training_data, valid_data and test_data must be True")
+
+
+    dataset = pickle.load(open(file_name,'rb'))
+    train_data = dataset['train']
+    valid_data = dataset['valid']
+    test_data = dataset['test']
+    if get_train_data:
+        return train_data
+    if get_valid_data:
+        return valid_data
+    if get_test_data:
+        return test_data
+
+
+
+def create_SRNN_model(session, forward_only, result_file=None, batch_size=None,same_param=False):
+    if batch_size is None:
+        batch_size = FLAGS.batch_size
+
+    model = SRNN_model(FLAGS.num_activities, FLAGS.num_frames, FLAGS.num_units, FLAGS.max_gradient_norm, batch_size, FLAGS.learning_rate,
+                                       FLAGS.learning_rate_decay_factor, FLAGS.adam_epsilon, FLAGS.GD, forward_only=forward_only, l2_regularization=FLAGS.l2_reg, weight_decay=FLAGS.reg_factor)
+    if not same_param:
+        checkpoint = tf.train.get_checkpoint_state(FLAGS.train_dir)
+        if checkpoint and tf.gfile.Exists(checkpoint.model_checkpoint_path):
+            if result_file is not None:
+                result_file.write("Continue training existing model! ")
+            print("Reading model parameters from %s" % checkpoint.model_checkpoint_path)
+            model.saver.restore(session, checkpoint.model_checkpoint_path)
+        else:
+            print("Created model with fresh parameters.")
+            session.run(tf.initialize_all_variables())
+    return model
+
+def main(_):
+    data = extract_features(FLAGS.data_file)
+    train_data, valid_data, test_data = data
+    result_file = open(FLAGS.train_dir + "/results.txt", 'a+')
+
+    with tf.Session() as sess:
+        with tf.name_scope("Train"):
+            result_file.write("Creating SRNN model \n")
+            with tf.variable_scope("Model"):
+                print("Creating SRNN model ")
+                # model = create_SRNN_model(sess,forward_only)
+
 
 if __name__ == '__main__':
-    index = sys.argv[1]
-    fold = sys.argv[2]
+	tf.app.run()
 
-    main_path = '/home/saiwen/sephora/master-thesis/activity-anticipation/'
-
-    path_to_dataset = '{1}/dataset/{0}'.format(fold, main_path)
-    path_to_checkpoints = '{1}/checkpoints/{0}'.format(fold, main_path)
-
-    if not os.path.exists(path_to_checkpoints):
-        os.mkdir(path_to_checkpoints)
-
-    test_data = cPickle.load(open('{1}/test_data_{0}.pik'.format(index, path_to_dataset)))
-    Y_te_human = test_data['labels_human']
-    Y_te_human_anticipation = test_data['labels_human_anticipation']
-    X_te_human_disjoint = test_data['features_human_disjoint']
-    X_te_human_shared = test_data['features_human_shared']
-
-    train_data = cPickle.load(open('{1}/train_data_{0}.pik'.format(index, path_to_dataset)))
-    Y_tr_human = train_data['labels_human']
-    Y_tr_human_anticipation = train_data['labels_human_anticipation']
-    X_tr_human_disjoint = train_data['features_human_disjoint']
-    X_tr_human_shared = train_data['features_human_shared']
-
-    Y_tr_objects = train_data['labels_objects']
-    Y_tr_objects_anticipation = train_data['labels_objects_anticipation']
-    X_tr_objects_disjoint = train_data['features_objects_disjoint']
-    X_tr_objects_shared = train_data['features_objects_shared']
-
-    num_sub_activities = int(np.max(Y_tr_human) - np.min(Y_tr_human) + 1)
-    num_affordances = int(np.max(Y_tr_objects) - np.min(Y_tr_objects) + 1)
-    num_sub_activities_anticipation = int(np.max(Y_tr_human_anticipation) - np.min(Y_tr_human_anticipation) + 1)
-    num_affordances_anticipation = int(np.max(Y_tr_objects_anticipation) - np.min(Y_tr_objects_anticipation) + 1)
-    inputJointFeatures = X_tr_human_shared.shape[2]
-    inputHumanFeatures = X_tr_human_disjoint.shape[2]
-    inputObjectFeatures = X_tr_objects_disjoint.shape[2]
-    assert (inputJointFeatures == X_tr_objects_shared.shape[2])
-
-    print '#human sub-activities ', num_sub_activities
-    print '#object affordances ', num_affordances
-    print '#human sub-activities-anticipation ', num_sub_activities_anticipation
-    print '#object affordances_anticipation ', num_affordances_anticipation
-    print 'shared features dim ', inputJointFeatures
-    print 'human features dim ', inputHumanFeatures
-    print 'object features dim ', inputObjectFeatures
-
-    epochs = 300
-    batch_size = X_tr_human_disjoint.shape[1]
-    learning_rate_decay = 0.97
-    decay_after = 5
-
-    use_pretrained = False
-    train_more = False
-
-    global rnn
-    if not use_pretrained:
-        if not os.path.exists('{1}/{0}/'.format(index, path_to_checkpoints)):
-            os.mkdir('{1}/{0}/'.format(index, path_to_checkpoints))
-
-        rnn = jointModel(num_sub_activities, num_affordances, inputJointFeatures, inputHumanFeatures,
-                         inputObjectFeatures)
-        rnn.train_model(X_tr_human_shared, X_tr_human_disjoint, Y_tr_human, X_tr_objects_shared,
-                     X_tr_objects_disjoint, Y_tr_objects, 1, '{1}/{0}/'.format(index, path_to_checkpoints), epochs,
-                     batch_size, learning_rate_decay, decay_after)
-
-    # else:
-    #     checkpoint = sys.argv[3]
-    #     # Prediction
-    #     rnn = load('{2}/{0}/checkpoint.{1}'.format(index, checkpoint, path_to_checkpoints))
-    #     if train_more:
-    #         rnn.train_model(X_tr_human_shared, X_tr_human_disjoint, 1, '{1}/{0}/'.format(index, path_to_checkpoints), epochs, batch_size,
-    #                      learning_rate_decay, decay_after)
